b3a80ba4be2e16deb1cba2553e55ed3bc056d62e
adabney3 <adabney3@vols.utk.edu>
2026-03-06 12:29:59 -0500

    toying with parameters

--- draw_ellipse/simple_ellipse.py
+++ draw_ellipse/simple_ellipse.py
@@ -2,12 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
+#I chose the smallest values of beta and alpha from the twiss file (line 73) because it produced the best ellipse
+#I tested with other values as well but larger beta and alpha values produced straight lines, not ellipses
 
 beta  =  0.00149999999999999 #semi-major axis
 #beta = 17057.11360944676198415 #testing with first values of beta and alpha in file; however, these values gave me a straight line not ellipse
+#beta = 409827.87423663376830518
+#beta = 722573.20524224289692938
 alpha =  0.00000000001248069 #alpha relates to tilt
 #alpha = -784.32981445063455794
+#alpha = 29970.80558916701193084
+#alpha = -7126.45070737356400059
 gamma = (1 + alpha**2) / beta  #semi-minor axis
 x = 0
 y = 0
